77f48af4de2d51c656cdcf62526446c33acffe67
Reinvy <reinvy.dev@gmail.com>
2024-12-02 17:11:44 +0700

    feat: "Updated variable declarations to use const, added IP whitelist and access control middleware"

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./app/routes/index');
-var storagesRouter = require('./app/routes/storages');
+const indexRouter = require('./app/routes/index');
+const storagesRouter = require('./app/routes/storages');
 
-var app = express();
+const whitelist = ["123.45.67.89", "98.76.54.32", "::1", "::ffff:127.0.0."]; // Ganti dengan IP Anda
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+
+    console.log(req.ip);
+    console.log(req.socket.remoteAddress);
+    console.log(req.headers['x-forwarded-for']);
+   
+    // Mendapatkan IP client
+    const clientIp = req.ip; 
+
+    // Logika untuk IPv6-mapped IPv4
+    const normalizedIp = clientIp.startsWith('::ffff:') ? clientIp.replace('::ffff:', '') : clientIp;
+
+    // Periksa apakah IP ada di whitelist
+    if (whitelist.includes(clientIp) || whitelist.includes(normalizedIp)) {
+        // Lanjutkan ke endpoint jika IP diizinkan
+        next(); 
+    } else {
+        return res.status(403).json({ 
+            success: false,
+            message: 'Access denied' 
+        });
+    }
+});
+  
 
 app.use('/', indexRouter);
 app.use('/api/v1/storages', storagesRouter);
