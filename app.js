066258da68377d9e8a81e054e3f5310714a6a0ce
Reinvy <reinvy.dev@gmail.com>
2024-12-03 14:53:30 +0700

    feat: added dotenv package and updated whitelist IP configuration to use environment variable.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -6,7 +8,7 @@ const logger = require('morgan');
 const indexRouter = require('./app/routes/index');
 const storagesRouter = require('./app/routes/storages');
 
-const whitelist = ["123.45.67.89", "98.76.54.32", "::1", "::ffff:127.0.0."]; // Ganti dengan IP Anda
+const whitelistIps = process.env.WHITELIST_IPS?.split(',') || []; // Ambil dan pecah daftar IP dari .env
 
 const app = express();
 
@@ -29,7 +31,7 @@ app.use((req, res, next) => {
     const normalizedIp = clientIp.startsWith('::ffff:') ? clientIp.replace('::ffff:', '') : clientIp;
 
     // Periksa apakah IP ada di whitelist
-    if (whitelist.includes(clientIp) || whitelist.includes(normalizedIp)) {
+    if (whitelistIps.includes(clientIp) || whitelistIps.includes(normalizedIp)) {
         // Lanjutkan ke endpoint jika IP diizinkan
         next(); 
     } else {
